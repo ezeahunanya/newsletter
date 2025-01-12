e7c1e8332202cc34691f3f4ff8bd2f1a42f6bbf0
Eze Ahunanya <ezeahunanya@outlook.com>
2025-01-12 22:49:10 +0000

    Add welcome email to lambda function

--- api/subscribe-api/email.mjs
+++ api/subscribe-api/email.mjs
@@ -56,7 +56,20 @@ export const sendWelcomeEmail = async (
       Body: {
         Html: {
           Data: `
-            <p>Welcome to the community! Complete your account setup <a href="${accountCompletionUrl}">here</a>.</p>
+            <html>
+              <body>
+                <p>Hey,</p>
+                <p>Thank you for verifying your email and joining me on this incredible journey!</p>
+                <p>My goal with this newsletter is simple: to help you discover and align with God’s plans for your life. These plans were established long before the foundations of the earth—plans that reflect His perfect will and purpose for you.
+                Whether you're just starting to ask questions or actively seeking direction, you're in the right place. Together, we’ll explore practical steps and biblical wisdom for you to step with confindence in the direction of your purpose.</p>
+                <p>As Proverbs 19:21 reminds us: “Many are the plans in a person’s heart, but it is the Lord’s purpose that prevails.”
+                Through this newsletter, I’ll share insights, resources, and encouragement to help you align your life with God’s purpose. I’d also love to hear from you—your story, questions, and struggles are all welcome. You can reach me at <strong><a href="mailto:contact@ezeahunanya.com">contact@ezeahunanya.com</a></strong>.</p>
+                <p>Also feel free to let me know your name with this link <a href="${accountCompletionUrl}">here</a>.</p>
+                <p>Let’s walk this journey together!</p>
+                <p>With faith and purpose,</p>
+                <p>Eze</p>
+              </body>
+            </html>
           `,
         },
       },
