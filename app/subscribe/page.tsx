cfdffb9c59b994e084cb191447c9763503384345
Eze Ahunanya <ezeahunanya@outlook.com>
2025-01-12 19:41:17 +0000

    Add background pattern and border space to layout for more uniform app

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -11,61 +11,45 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="relative isolate min-h-screen overflow-hidden bg-gray-100 py-16 sm:py-24 lg:py-32 dark:bg-gray-900">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
-          <div className="max-w-xl lg:max-w-lg">
-            <h2 className="max-w-xl text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl lg:col-span-7 dark:text-white">
-              Subscribe to my newsletter
-            </h2>
-            <SubscribeForm />
+    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+      <div className="max-w-xl lg:max-w-lg">
+        <h2 className="max-w-xl text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl lg:col-span-7 dark:text-white">
+          Subscribe to my newsletter
+        </h2>
+        <SubscribeForm />
+      </div>
+      <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+        <div className="flex flex-col items-start">
+          <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+            <CalendarDaysIcon
+              aria-hidden="true"
+              className="size-6 text-gray-700 dark:text-white"
+            />
           </div>
-          <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
-            <div className="flex flex-col items-start">
-              <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                <CalendarDaysIcon
-                  aria-hidden="true"
-                  className="size-6 text-gray-700 dark:text-white"
-                />
-              </div>
-              <dt className="mt-4 text-base font-semibold text-gray-900 dark:text-white">
-                Updates That Matter
-              </dt>
-              <dd className="mt-2 text-base/7 text-gray-900 dark:text-gray-400">
-                I’ll share insights and ideas only when they’re genuinely worth
-                your time. No spam, no filler. Just value!
-              </dd>
-            </div>
-            <div className="flex flex-col items-start">
-              <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                <ChatBubbleLeftRightIcon
-                  aria-hidden="true"
-                  className="size-6 text-gray-700 dark:text-white"
-                />
-              </div>
-              <dt className="mt-4 text-base font-semibold text-gray-900 dark:text-white">
-                Start a Conversation
-              </dt>
-              <dd className="mt-2 text-base/7 text-gray-900 dark:text-gray-400">
-                When you subscribe, you’ll receive an email address to reach me.
-                I’d love to hear from you. Let’s start a conversation!
-              </dd>
-            </div>
-          </dl>
+          <dt className="mt-4 text-base font-semibold text-gray-900 dark:text-white">
+            Updates That Matter
+          </dt>
+          <dd className="mt-2 text-base/7 text-gray-900 dark:text-gray-400">
+            I’ll share insights and ideas only when they’re genuinely worth your
+            time. No spam, no filler. Just value!
+          </dd>
         </div>
-      </div>
-      <div
-        aria-hidden="true"
-        className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6"
-      >
-        <div
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-        />
-      </div>
+        <div className="flex flex-col items-start">
+          <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+            <ChatBubbleLeftRightIcon
+              aria-hidden="true"
+              className="size-6 text-gray-700 dark:text-white"
+            />
+          </div>
+          <dt className="mt-4 text-base font-semibold text-gray-900 dark:text-white">
+            Start a Conversation
+          </dt>
+          <dd className="mt-2 text-base/7 text-gray-900 dark:text-gray-400">
+            When you subscribe, you’ll receive an email address to reach me. I’d
+            love to hear from you. Let’s start a conversation!
+          </dd>
+        </div>
+      </dl>
     </div>
   );
 }
