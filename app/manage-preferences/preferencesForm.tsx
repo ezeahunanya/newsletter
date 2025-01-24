a72f565046cd62efbe5ec2538dbcb24ee777d602
Eze Ahunanya <ezeahunanya@outlook.com>
2025-01-24 21:15:07 +0000

    Dynamically load preferences page with the preferences stored in the database
    Also a descriptions for each checkbox

--- app/manage-preferences/preferencesForm.tsx
+++ app/manage-preferences/preferencesForm.tsx
@@ -16,6 +16,13 @@ if (!apiBaseUrl || !managePreferencesPath) {
 
 const managePreferencesUrl = `${apiBaseUrl}${managePreferencesPath}`;
 
+const PREFERENCE_DESCRIPTIONS = {
+  updates: "Receive valuable insights and ideas through new YouTube videos.",
+  promotions:
+    "Access exclusive offers designed to bring you real value and benefit.",
+  // Add more preferences and descriptions here as needed
+};
+
 interface PreferencesFormProps {
   initialPreferences: {
     promotions: boolean;
@@ -25,8 +32,7 @@ interface PreferencesFormProps {
 }
 
 interface FormValues {
-  promotions: boolean;
-  updates: boolean;
+  [key: string]: boolean; // Dynamic keys for preferences
 }
 
 export default function PreferencesForm({
@@ -78,108 +84,82 @@ export default function PreferencesForm({
   };
 
   const handleUnsubscribeAll = () => {
-    setValue("promotions", false);
-    setValue("updates", false);
+    Object.keys(initialPreferences).forEach((key) => {
+      setValue(key, false);
+    });
   };
 
   const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, checked } = e.target;
+    setValue(name, checked);
+  };
 
-    setValue(name as "promotions" | "updates", checked);
+  const renderPreferences = (preferences: { [key: string]: boolean }) => {
+    return Object.entries(preferences).map(([key]) => (
+      <div key={key} className="flex gap-3">
+        <div className="flex h-6 shrink-0 items-center">
+          <div className="group grid size-4 grid-cols-1">
+            <input
+              id={key}
+              type="checkbox"
+              {...register(key)}
+              onChange={handleCheckboxChange}
+              checked={watch(key)}
+              className="col-start-1 row-start-1 appearance-none rounded border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:border-gray-700 dark:bg-gray-800 dark:checked:border-indigo-500 dark:checked:bg-indigo-500 dark:focus-visible:outline-indigo-500"
+            />
+            <svg
+              fill="none"
+              viewBox="0 0 14 14"
+              className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white"
+            >
+              <path
+                d="M3 8L6 11L11 3.5"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className={`opacity-0 ${
+                  currentValues[key] ? "opacity-100" : ""
+                }`}
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="text-sm/6">
+          <label
+            htmlFor={key}
+            className="font-medium text-gray-900 dark:text-white"
+          >
+            {key.charAt(0).toUpperCase() + key.slice(1)}
+          </label>
+          <p className="text-gray-500 dark:text-gray-400">
+            {
+              PREFERENCE_DESCRIPTIONS[
+                key as keyof typeof PREFERENCE_DESCRIPTIONS
+              ]
+            }
+          </p>
+        </div>
+      </div>
+    ));
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="mx-auto mt-8 max-w-xs">
+    <form onSubmit={handleSubmit(onSubmit)} className="mx-auto mt-8 max-w-lg">
       <fieldset>
         <legend className="sr-only">Preferences</legend>
         <h1 className="mb-4 text-base/7 font-bold text-gray-900 dark:text-white">
           Preferences
         </h1>
         <div className="space-y-6">
-          {/* Updates Checkbox */}
-          <div className="flex gap-3">
-            <div className="flex h-6 shrink-0 items-center">
-              <div className="group grid size-4 grid-cols-1">
-                <input
-                  id="updates"
-                  type="checkbox"
-                  {...register("updates")}
-                  onChange={handleCheckboxChange}
-                  checked={watch("updates")}
-                  className="col-start-1 row-start-1 appearance-none rounded border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:border-gray-700 dark:bg-gray-800 dark:checked:border-indigo-500 dark:checked:bg-indigo-500 dark:focus-visible:outline-indigo-500"
-                />
-                <svg
-                  fill="none"
-                  viewBox="0 0 14 14"
-                  className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white"
-                >
-                  <path
-                    d="M3 8L6 11L11 3.5"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className={`opacity-0 ${
-                      currentValues.updates ? "opacity-100" : ""
-                    }`}
-                  />
-                </svg>
-              </div>
-            </div>
-            <div className="text-sm/6">
-              <label
-                htmlFor="updates"
-                className="font-medium text-gray-900 dark:text-gray-300"
-              >
-                Receive Updates
-              </label>
-            </div>
-          </div>
-
-          {/* Promotions Checkbox */}
-          <div className="flex gap-3">
-            <div className="flex h-6 shrink-0 items-center">
-              <div className="group grid size-4 grid-cols-1">
-                <input
-                  id="promotions"
-                  type="checkbox"
-                  {...register("promotions")}
-                  onChange={handleCheckboxChange}
-                  checked={watch("promotions")}
-                  className="col-start-1 row-start-1 appearance-none rounded border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:border-gray-700 dark:bg-gray-800 dark:checked:border-indigo-500 dark:checked:bg-indigo-500 dark:focus-visible:outline-indigo-500"
-                />
-                <svg
-                  fill="none"
-                  viewBox="0 0 14 14"
-                  className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white"
-                >
-                  <path
-                    d="M3 8L6 11L11 3.5"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className={`opacity-0 ${
-                      currentValues.promotions ? "opacity-100" : ""
-                    }`}
-                  />
-                </svg>
-              </div>
-            </div>
-            <div className="text-sm/6">
-              <label
-                htmlFor="promotions"
-                className="font-medium text-gray-900 dark:text-gray-300"
-              >
-                Receive Promotions
-              </label>
-            </div>
-          </div>
-
-          {/* Unsubscribe All Button */}
+          {renderPreferences(initialPreferences)}{" "}
+          {/* Dynamically render preferences */}
           <Button
             type="button"
             variant="secondary"
             onClick={handleUnsubscribeAll}
-            disabled={!watch("updates") && !watch("promotions")}
+            disabled={Object.keys(initialPreferences).every(
+              (key) => !watch(key),
+            )}
           >
             Unsubscribe From All
           </Button>
