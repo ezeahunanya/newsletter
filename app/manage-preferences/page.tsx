1c0b739956e9469b5bc61c87ebdc03afb2741183
Eze Ahunanya <ezeahunanya@outlook.com>
2025-01-26 20:23:21 +0000

    Add alert component to preferences page

--- app/manage-preferences/page.tsx
+++ app/manage-preferences/page.tsx
@@ -1,10 +1,8 @@
 import { Metadata } from "next";
 import PreferencesForm from "./preferencesForm";
-import {
-  ExclamationTriangleIcon,
-  XCircleIcon,
-} from "@heroicons/react/24/outline";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { getPreferences } from "./getPreference";
+import Alert from "../components/ui/alert";
 
 export const metadata: Metadata = {
   title: "Manage Preferences",
@@ -20,20 +18,7 @@ export default async function PreferencesPage({
   const { token } = await searchParams;
 
   if (!token) {
-    return (
-      <div className="rounded-md bg-red-50 p-4">
-        <div className="flex">
-          <div className="shrink-0">
-            <XCircleIcon aria-hidden="true" className="size-5 text-red-400" />
-          </div>
-          <div className="ml-3">
-            <h3 className="text-sm font-medium text-red-800">
-              Invalid request: Token is missing.
-            </h3>
-          </div>
-        </div>
-      </div>
-    );
+    return <Alert type="error" message="Invalid request: Token is missing." />;
   }
 
   const preferencesData = await getPreferences(token);
