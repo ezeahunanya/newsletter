984aa200db59de03d24818a5681e10bd69c68a8e
Eze Ahunanya <ezeahunanya@outlook.com>
2025-03-16 12:45:22 +0000

    Add responses in email api

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -3,26 +3,38 @@ import {
   sendWelcomeEmail,
   sendRegeneratedTokenEmail,
 } from "@/lib/send-emails/email";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const body = await req.json();
   const { email, type, data } = body;
 
+  let message = "";
+
   try {
     if (type === "verify") {
       await sendVerificationEmail(email, data.verificationUrl);
+      message = "Verification email sent successfully!";
     } else if (type === "welcome") {
       await sendWelcomeEmail(
         email,
         data.accountCompletionUrl,
         data.preferencesUrl,
       );
+      message = "Welcome email sent successfully!";
     } else if (type === "regenerate") {
       await sendRegeneratedTokenEmail(email, data.linkUrl, data.origin);
     } else {
       throw new Error("Unknown email type");
     }
-  } catch (error) {
+    return NextResponse.json({ success: true, message });
+  } catch (error: unknown) {
     console.error("❌ Error sending email:", error);
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { success: false, message: error.message },
+        { status: 400 },
+      );
+    }
   }
 }
